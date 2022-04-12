35664610eba3783fa882f659d9b7201916c0d60d
Rink Springer <rink@rink.nu>
2022-04-12 07:51:13 +0200

    Proper license this content

--- BrowseDialog.cpp
+++ BrowseDialog.cpp
@@ -1,3 +1,9 @@
+/*-
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * Copyright (c) 2022 Rink Springer <rink@rink.nu>
+ * For conditions of distribution and use, see LICENSE file
+ */
 #include "BrowseDialog.h"
 #include "MTP.h"
 #include <QStandardItemModel>
